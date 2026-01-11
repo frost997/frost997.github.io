ac237ad02e982a4fb28b2acd4a7df89066d97ede
Frost997 <nguyentranduckhai0@gmail.com>
2026-01-11 17:19:20 +0700

    revert

--- src/app.d.ts
+++
@@ -1,9 +0,0 @@
-declare global {
-	namespace App {
-		interface Locals {
-			user?: import('$types').User;
-		}
-	}
-}
-
-export {};
--- src/lib/components/layout/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Header } from './Header.svelte';
-export { default as Footer } from './Footer.svelte';
-export { default as AdminSidebar } from './AdminSidebar.svelte';
-export { default as AdminHeader } from './AdminHeader.svelte';
